63dea2a433b4ab0420e5ab2d882b38498940b2bf
Chunyang Xu <chunyang@macports.org>
2015-06-16 07:48:50 +0000

    users/chunyang: port.el: Create port-menu-mode
    git-svn-id: https://svn.macports.org/repository/macports/users/chunyang@137649 d073be05-634f-4543-b044-5fe20cf6d1d6

--- port.el/port.el
+++ port.el/port.el
@@ -2,7 +2,9 @@
 
 ;; Copyright (C) 2015  Chunyang Xu
 
-;; Author: Chunyang Xu <xuchunyang56@gmail.com>
+;; Author: Chunyang Xu <chunyang@macports.org>
+;; Version: 0.01
+;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: MacPorts
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -20,11 +22,34 @@
 
 ;;; Commentary:
 
-;;
+;; Latest version can be found at:
+;; https://svn.macports.org/repository/macports/users/chunyang/port.el/
 
 ;;; Code:
 
+(require 'tabulated-list)
 
+(define-derived-mode port-menu-mode tabulated-list-mode "MacPorts Port Menu"
+  "Major mode browsing a list of ports.
+Letters do not insert themselves; instead, they are commands. "
+  (setq tabulated-list-format `[("Package" 18 t)
+                                ("Version" 13 nil)
+                                ("Status"  10 t)
+                                ("Description" 0 nil)])
+  (setq tabulated-list-padding 2)
+  (setq tabulated-list-sort-key (cons "Status" nil))
+  (tabulated-list-init-header))
+
+;;;###autoload
+(defun port-list-ports ()
+  (interactive)
+  (let ((buf (get-buffer-create "*Ports*")))
+    (with-current-buffer buf
+      (port-menu-mode)
+      (setq tabulated-list-entries
+            (list (list "1" ["emacs" "24.5" "installed" "The GNU Emacs text editor"])))
+      (tabulated-list-print t)
+      (switch-to-buffer buf))))
 
 (provide 'port)
 ;;; port.el ends here
